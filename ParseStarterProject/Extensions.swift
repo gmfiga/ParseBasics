68bde4a40ab91e9823e0ba84f2acd3bf5fb2f8b2
Administrator <administrator@gbtc408013dt.montgomerycollege.edu>
2018-06-28 16:35:15 -0400

    this version contains the basic username and login interface

---
+++ ParseStarterProject/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  ParseBasics
+//
+//  Created by Administrator on 6/28/18.
+//  Copyright © 2018 Parse. All rights reserved.
+//
+
+
---
+++ ParseStarterProject/LoggedInViewController.swift
@@ -0,0 +1,75 @@
+//
+//  LoggedInViewController.swift
+//  ParseBasics
+//
+//  Created by Administrator on 6/28/18.
+//  Copyright © 2018 Parse. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoggedInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logoutOfApp(_ sender: UIButton) {
+        let sv = UIViewController.displaySpinner(onView: self.view)
+        PFUser.logOutInBackground { (error: Error?) in
+            UIViewController.removeSpinner(spinner: sv)
+            if (error == nil){
+                self.loadLoginScreen()
+            }else{
+                if let descrip = error?.localizedDescription{
+                    self.displayMessage(message: descrip)
+                }else{
+                    self.displayMessage(message: "error logging out")
+                }
+                
+            }
+        }
+    }
+    
+    
+    
+    func displayMessage(message:String) {
+        let alertView = UIAlertController(title: "Error!", message: message, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+        }
+        alertView.addAction(OKAction)
+        if let presenter = alertView.popoverPresentationController {
+            presenter.sourceView = self.view
+            presenter.sourceRect = self.view.bounds
+        }
+        self.present(alertView, animated: true, completion:nil)
+    }
+    
+    func loadLoginScreen(){
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyBoard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
